24721edab703c50f209eb60d4b38ab953f341e70
fagner02 <fagnerrc12@gmail.com>
2021-11-29 17:18:57 -0300

    Update card layout and function

--- lib/info_card.dart
+++ lib/info_card.dart
@@ -5,19 +5,23 @@ class InfoCard extends StatefulWidget {
   final String? text;
   final Widget child;
   final double? height;
+  final double? width;
   final EdgeInsets? padding;
   final EdgeInsets? margin;
   final CrossAxisAlignment? horizontalAlign;
   final double? fontSize;
+  final void Function()? onTap;
   const InfoCard(
       {Key? key,
       this.text,
       this.height,
+      this.width,
       this.child = const LimitedBox(),
       this.padding,
       this.margin,
       this.horizontalAlign,
-      this.fontSize})
+      this.fontSize,
+      this.onTap})
       : super(key: key);
 
   @override
@@ -28,39 +32,43 @@ class CardState extends State<InfoCard> {
   @override
   Widget build(BuildContext context) {
     // Card Box
-    return Container(
-      margin: widget.margin ?? const EdgeInsets.symmetric(vertical: 10),
-      padding: widget.padding ?? const EdgeInsets.all(20),
-      height: widget.height,
-      width: MediaQuery.of(context).size.width * 0.8,
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(
-            Radius.circular(20),
-          ),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black12,
-                offset: Offset(0, 0),
-                blurRadius: 10,
-                spreadRadius: 1)
-          ]),
-      // Children
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: widget.horizontalAlign ?? CrossAxisAlignment.start,
-        children: [
-          // Text
-          if (widget.text != null)
-            Text(
-              widget.text ?? '',
-              style: TextStyle(
-                  fontSize: widget.fontSize ?? 20,
-                  color: AppTheme.cardTitleColor),
+    return GestureDetector(
+      onTap: widget.onTap,
+      child: Container(
+        margin: widget.margin ?? const EdgeInsets.symmetric(vertical: 10),
+        padding: widget.padding ?? const EdgeInsets.all(20),
+        height: widget.height,
+        width: widget.width ?? MediaQuery.of(context).size.width * 0.8,
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
             ),
-          // Child parameter
-          widget.child,
-        ],
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0, 0),
+                  blurRadius: 10,
+                  spreadRadius: 1)
+            ]),
+        // Children
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment:
+              widget.horizontalAlign ?? CrossAxisAlignment.start,
+          children: [
+            // Text
+            if (widget.text != null)
+              Text(
+                widget.text ?? '',
+                style: TextStyle(
+                    fontSize: widget.fontSize ?? 20,
+                    color: AppTheme.cardTitleColor),
+              ),
+            // Child parameter
+            widget.child,
+          ],
+        ),
       ),
     );
   }
