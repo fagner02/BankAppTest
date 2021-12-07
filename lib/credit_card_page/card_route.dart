9f5277301f7c1a4d9529025fecb81262fbeca41c
fagner02 <fagnerrc12@gmail.com>
2021-12-07 17:52:31 -0300

    Add Splash Screen

--- lib/credit_card_page/card_route.dart
+++ lib/credit_card_page/card_route.dart
@@ -1,6 +1,7 @@
 import '../home_page.dart';
 import 'card_page.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
 class CreditCardRoute extends PageRouteBuilder {
   CreditCardRoute()
@@ -8,13 +9,16 @@ class CreditCardRoute extends PageRouteBuilder {
           pageBuilder: (context, animation, secondaryAnimation) =>
               const CreditCard(),
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return SlideTransition(
-              position: animation.drive(
-                Tween<Offset>(
-                  begin: const Offset(0.0, 1.0),
-                  end: Offset.zero,
-                ).chain(CurveTween(curve: Curves.elasticOut)),
-              ),
+            return ElasticIn(
+              animate: true,
+              duration: Duration(milliseconds: 300),
+              // position: animation.drive(
+              //   Tween<Offset>(
+              //     begin: const Offset(0.0, 1.0),
+              //     end: Offset.zero,
+              //   ).chain(CurveTween(curve: Curves.elasticOut)),
+              // ),
+
               child: child,
             );
           },
